f282842c6b023f5614e169330b26887ec0f6fa25
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:16:23 +0100

    Update page.js

---
+++ app/maintenance/page.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Site en maintenance</h1>
+      <p>Nous revenons bientôt.</p>
+    </div>
+  );
+}
