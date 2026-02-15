9c63848b341d00e6168a14a786b1295778e8cd22
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:41:32 +0100

    Update page.js

---
+++ app/securite/page.js
@@ -0,0 +1,8 @@
+export default function Securite() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Sécurité</h1>
+      <p>Nous garantissons des solutions de sécurité fiables et modernes.</p>
+    </div>
+  );
+}
