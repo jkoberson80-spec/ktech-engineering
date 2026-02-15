0655223cbb9dedb377d127d60ba75a0fabe8edc0
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:36:49 +0100

    Update page.js

---
+++ app/materiel/page.js
@@ -0,0 +1,8 @@
+export default function Materiel() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Matériel</h1>
+      <p>Page en cours de construction.</p>
+    </div>
+  );
+}
