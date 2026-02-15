9112d95c16e5a5d7794bc3b5011782a06b18f14a
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:57:31 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,24 @@
- export default function Home() {
+ import Link from "next/link";
+
+export default function Home() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div style={{ padding: "40px", textAlign: "center" }}>
       <h1>Ktech Engineering</h1>
       <p>Bienvenue sur le site officiel.</p>
+
+      <div style={{ marginTop: "30px" }}>
+        <Link href="/services">
+          <button style={{ margin: "10px" }}>Services</button>
+        </Link>
+
+        <Link href="/securite">
+          <button style={{ margin: "10px" }}>Sécurité</button>
+        </Link>
+
+        <Link href="/contact">
+          <button style={{ margin: "10px" }}>Contact</button>
+        </Link>
+      </div>
     </div>
   );
 }
