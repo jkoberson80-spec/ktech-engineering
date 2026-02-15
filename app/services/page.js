c352de1143a8975f1fbcb4bb8b1af5f7a4876c1e
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:40:46 +0100

    Update page.js

---
+++ app/services/page.js
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Nos Services</h1>
+      <p>Découvrez nos solutions et prestations professionnelles.</p>
+    </div>
+  );
+}
