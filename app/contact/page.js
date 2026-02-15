be44001030dd6a38ac3ef73a77fa0e413568cfc3
Ktech-engineering <jkoberson80@gmail.com>
2026-02-15 06:29:52 +0100

    Update page.js

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Contact</h1>
+      <p>Contactez-nous à contact@ktech-engineering.com</p>
+    </div>
+  );
+}
