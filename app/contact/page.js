5d7d4828716bdac335436271a63bcfddc86c81a7
Ktech-engineering <jkoberson80@gmail.com>
2026-02-23 00:00:28 +0100

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,36 @@ export default function Contact() {
   return (
     <div style={{ padding: "40px", textAlign: "center" }}>
       <h1>Contact</h1>
-      <p>Contactez-nous à contact@ktech-engineering.com</p>
+      {/* Email */}
+<p style={{ marginBottom: "15px" }}>
+  <a
+    href="mailto:contact@ktech-engineering.com"
+    style={{
+      color: "#1FA2B8",
+      textDecoration: "none",
+      fontWeight: "600",
+      fontSize: "16px",
+    }}
+  >
+    📧 contact@ktech-engineering.com
+  </a>
+</p>
+
+{/* Téléphone */}
+<p style={{ marginBottom: "15px" }}>
+  <a
+    href="tel:+237677612062"
+    style={{
+      color: "#1FA2B8",
+      textDecoration: "none",
+      fontWeight: "600",
+      fontSize: "16px",
+    }}
+  >
+    📞 +237 677 612 062
+  </a>
+</p>
     </div>
   );
 }
+
